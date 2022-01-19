38a36c374ee3e4946faef445663f39b9e0f9e1ec
yoyu <atp6u4m4a@gmail.com>
2022-01-20 02:53:52 +0800

    adjust card picture size

--- src/component/card.js
+++ src/component/card.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Card = (props) => {
+  const style = {
+    height: '80%',
+    backgroundColor: 'black'
+  }
   return (
     <div className='card' onClick={props.randomFunction}>
-      <div className='pic' />
+      <div className='pic' style={style} />
       <div className='characterName'>{props.characterName}</div>
     </div>
   )
--- src/component/field.js
+++ src/component/field.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Card from './card';
 
 const Field = (props) => {
-  return (          
-    <div className='field'>
+  const style = {
+    height: '20%',
+  }
+  return (            
+    <div className='field' style={style}>
       {props.names.map(
         name => <Card characterName={name} randomFunction={(e) => props.randomFunction(e)}/>)}
     </div>
